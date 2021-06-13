aaa8adab97e4b727fbf8b58266cc3d64848a701b
austinhjones3 <austinhjones3@gmail.com>
2021-06-13 17:32:24 -0500

    Navbar in progress

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,15 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+// import { Navbar, Container, Nav } from "react-bootstrap";
 export default function Header() {
   return (
     <>
-      <nav>
-        <Link to="/" className=""></Link>
-        <Link to="" className=""></Link>
-        <Link to="" className=""></Link>
-        <Link to="" className=""></Link>
-        <Link to="" className=""></Link>
-      </nav>
+      {/* <nav>
+        <Link to="/" className="">
+          Home
+        </Link>
+        <Link to="/about" className="">
+          About Me
+        </Link>
+        <Link to="/projects" className="">
+          Projects
+        </Link>
+        <Link to="/resume" className="">
+          Resume
+        </Link>
+        <Link to="/contact" className="">
+          Contact Me
+        </Link>
+      </nav> */}
+
+      <Navbar bg="light" expand="sm">
+        <h3>Austin Jones</h3>
+        <h4>Full Stack Developer</h4>
+        <Container>
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link to="/" className="">
+                Home
+              </Link>
+              <Link to="/about" className="ml-3">
+                About Me
+              </Link>
+              <Link to="/projects" className="ml-3">
+                Projects
+              </Link>
+              <Link to="/resume" className="ml-3">
+                Resume
+              </Link>
+              <Link to="/contact" className="ml-3">
+                Contact Me
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 }
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import BrowserRouter from "react-router-dom/BrowserRouter";
+import Container from "react-bootstrap/Container";
 import Header from "./Header";
 import Routes from "./Routes";
 import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <BrowserRouter>
-      <main className="container">
-        <div>
-          <Header />
-        </div>
-        <div>
-          <Routes />
-        </div>
-        <div>
-          <Footer />
-        </div>
-      </main>
-    </BrowserRouter>
+    <Container>
+      <BrowserRouter>
+        <main className="container">
+          <div>
+            <Header />
+          </div>
+          <div>
+            <Routes />
+          </div>
+          <div>
+            <Footer />
+          </div>
+        </main>
+      </BrowserRouter>
+    </Container>
   );
 }
