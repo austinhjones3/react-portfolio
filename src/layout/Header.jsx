9638186a28caf1049a526cb40a06837f167cc90e
austinhjones3 <austinhjones3@gmail.com>
2021-06-13 19:32:33 -0500

    making progress on header and color scheme

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,62 +1,39 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import "./Header.css";
 // import { Navbar, Container, Nav } from "react-bootstrap";
 export default function Header() {
   return (
     <>
-      {/* <nav>
-        <Link to="/" className="">
-          Home
-        </Link>
-        <Link to="/about" className="">
-          About Me
-        </Link>
-        <Link to="/projects" className="">
-          Projects
-        </Link>
-        <Link to="/resume" className="">
-          Resume
-        </Link>
-        <Link to="/contact" className="">
-          Contact Me
-        </Link>
-      </nav> */}
-      <Row className="mt-2">
-        <Col></Col>
-      </Row>
-      <Navbar bg="light" expand="sm">
-        <Row>
+      <h2 className="d-flex justify-content-center">Austin Jones</h2>
+      <h4 className="d-flex justify-content-center">Full Stack Developer</h4>
+      <div className="d-flex justify-content-center">
+        <Navbar className="text-center" expand="md">
           <Container>
-            <h4>Austin Jones | Full Stack Developer</h4>
-
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Link to="/" className="">
+              <Nav className="me-auto text-center" pullRight>
+                <Nav.Link as={Link} to="/" className="">
                   Home
-                </Link>
-                <Link to="/about" className="">
+                </Nav.Link>
+                <Nav.Link as={Link} to="/about" className="">
                   About Me
-                </Link>
-                <Link to="/projects" className="">
+                </Nav.Link>
+                <Nav.Link as={Link} to="/projects" className="">
                   Projects
-                </Link>
-                <Link to="/resume" className="">
+                </Nav.Link>
+                <Nav.Link as={Link} to="/resume" className="">
                   Resume
-                </Link>
-                <Link to="/contact" className="">
+                </Nav.Link>
+                <Nav.Link as={Link} to="/contact" className="">
                   Contact Me
-                </Link>
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
-        </Row>
-      </Navbar>
+        </Navbar>
+      </div>
     </>
   );
 }
