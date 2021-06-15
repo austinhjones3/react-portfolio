8fecea58fd0af5f6c487c7d22b390626909a1e2c
austinhjones3 <austinhjones3@gmail.com>
2021-06-15 15:49:50 -0500

    resume page done

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import Header from "./Header";
 import Routes from "./Routes";
 import Footer from "./Footer";
+import "../styles/Header.css";
 
 export default function Layout() {
   return (
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import "../styles/Resume.css";
+import { ImDownload } from "react-icons/im";
+import resumeImg from "../images/austin-jones-resume.jpg";
+
 export default function Resume() {
   return (
-    <>
-      <a
-        href="./austin-jones-resume.pdf"
-        class="btn btn-primary mt-5 text-center mx-auto"
-        download
-      >
-        Download Resume
-      </a>
-    </>
+    <div className="resume-container">
+      <div className="row">
+        <center className="col-12">
+          <a
+            href="./austin-jones-resume.pdf"
+            class="btn mt-4 text-center download-resume  mx-auto"
+            download
+          >
+            <ImDownload /> Download Resume
+          </a>
+        </center>
+        <img src={resumeImg} alt="my resume!" className="mx-auto mt-4" />
+      </div>
+    </div>
   );
 }
