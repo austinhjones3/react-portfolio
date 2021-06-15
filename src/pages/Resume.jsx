2af63341059eaf7cc2b3c68e66c100a0d666de2f
austinhjones3 <austinhjones3@gmail.com>
2021-06-15 15:53:35 -0500

    resume page done

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,18 +5,18 @@ import resumeImg from "../images/austin-jones-resume.jpg";
 
 export default function Resume() {
   return (
-    <div className="resume-container">
+    <div className="container resume-container">
       <div className="row">
         <center className="col-12">
           <a
             href="./austin-jones-resume.pdf"
-            class="btn mt-4 text-center download-resume  mx-auto"
+            class="btn mt-4 text-center download-resume mx-auto"
             download
           >
             <ImDownload /> Download Resume
           </a>
         </center>
-        <img src={resumeImg} alt="my resume!" className="mx-auto mt-4" />
+        <img src={resumeImg} alt="my resume!" className="w-100 mx-auto mt-4" />
       </div>
     </div>
   );
