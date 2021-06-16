9078a520a00008a9774d3653d6e9ed35bab525ef
Austin Jones <75518099+austinhjones3@users.noreply.github.com>
2021-06-15 23:24:42 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
         <div align="center" className="mx-auto col-12">
           <p className="home-pitch mt-3">
             From building REST API's, to React Single Page Applications, or solving
-            general problems and scriping in JavaScript, I'm the person for the job.
+            general problems and scripting in JavaScript, I'm the person for the job.
           </p>
         </div>
       </div>
